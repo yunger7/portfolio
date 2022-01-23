8cf2bd525a2bcab3f2f4f9d9841133836b6bccf8
yunger <luisgalete6264@gmail.com>
2022-01-23 11:22:54 -0300

    Create basic header layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,85 @@
 import Head from "next/head";
-import { Typography, Container, Box } from "@mui/material";
+import Image from "next/image";
+import { Typography, Container, Box, useTheme } from "@mui/material";
+
+import { WaveDivider } from "../components";
+import { palette } from "../styles/theme";
+
+import BackgroundImage from "../../public/background.jpg";
 
 export default function Home() {
+	const theme = useTheme();
+
 	return (
-		<Container maxWidth="lg" sx={{ pt: 5 }}>
+		<Box>
 			<Head>
 				<title>Luís Galete | Home</title>
 			</Head>
-			<Typography variant="h1" align="center">
-				Hello, my{" "}
-				<Box component="span" sx={{ color: "primary.main" }}>
-					cool
-				</Box>{" "}
-				world!
-			</Typography>
-		</Container>
+			<Box
+				sx={{
+					position: "relative",
+					width: "100vw",
+					maxWidth: "100%",
+					height: "100vh",
+					maxHeight: "100%",
+					overflow: "hidden",
+					zIndex: -1,
+
+					"& img": {
+						zIndex: -1,
+					},
+
+					"& > span": {
+						backgroundColor: "rgba(0, 0, 0, 0.1) !important",
+					},
+				}}
+			>
+				<Image
+					priority
+					src={BackgroundImage}
+					layout="fill"
+					objectFit="cover"
+					quality={100}
+					placeholder="blur"
+				/>
+				<Box
+					sx={{
+						position: "absolute",
+						left: "10%",
+						top: "25%",
+						color: palette.nord6,
+					}}
+				>
+					<Typography variant="h1">Luís Galete</Typography>
+					<Typography component="p" variant="h2">
+						Is building{" "}
+						<Box component="span" sx={{ color: "primary.main" }}>
+							cool
+						</Box>{" "}
+						things
+					</Typography>
+				</Box>
+				<WaveDivider
+					invert
+					position="bottom"
+					height={150}
+					width={125}
+					color={theme.palette.background.default}
+				/>
+			</Box>
+			<Container component="main" maxWidth="lg" sx={{ pb: 10 }}>
+				<Typography variant="h2">I have the CONTENT</Typography>
+				<Typography variant="body1">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
+					debitis vel expedita nam nesciunt sit voluptas? Dignissimos
+					consectetur suscipit nam, quam ratione hic doloremque. Ullam tempora
+					sunt velit voluptatibus numquam. Lorem ipsum dolor sit amet
+					consectetur adipisicing elit. Asperiores vero odit repellendus maiores
+					quibusdam expedita ipsam atque neque provident recusandae, laboriosam,
+					molestiae perspiciatis repudiandae, explicabo iusto? Explicabo at ex
+					dolorem?
+				</Typography>
+			</Container>
+		</Box>
 	);
 }
