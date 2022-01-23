9bc3c42561c4daa9820e3f767aaba7cb0e030116
yunger <luisgalete6264@gmail.com>
2022-01-23 10:43:42 -0300

    Add favicon and title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import { Typography, Container, Box } from "@mui/material";
 
 export default function Home() {
 	return (
 		<Container maxWidth="lg" sx={{ pt: 5 }}>
+			<Head>
+				<title>Luís Galete | Home</title>
+			</Head>
 			<Typography variant="h1" align="center">
 				Hello, my{" "}
 				<Box component="span" sx={{ color: "primary.main" }}>
