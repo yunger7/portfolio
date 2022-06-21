ab8377c36a7d725c371fc279d347d211894cccac
yunger <luisgalete6264@gmail.com>
2022-06-21 18:02:47 -0300

    feat(*): configure mantine in project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
-}
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
-export default MyApp;
+export default function App({ Component, pageProps }) {
+	return (
+		<>
+			<Head>
+				<title>Luís Galete | Portfolio</title>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+
+			<MantineProvider
+				withGlobalStyles
+				withNormalizeCSS
+				theme={{
+					colorScheme: "light",
+				}}
+			>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</>
+	);
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,22 @@
+import { Container, Paper, Title, Text, Button, Divider } from "@mantine/core";
+
 export default function Home() {
-	return <h1>Hello world!</h1>;
+	return (
+		<Container size="xl" p="xl">
+			<Paper withBorder shadow="lg" p="lg">
+				<Title order={1} sx={theme => ({ marginBottom: theme.spacing.sm })}>
+					Hello World!
+				</Title>
+				<Text>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut possimus
+					libero, aliquam soluta labore adipisci totam explicabo voluptatibus
+					nostrum aut harum omnis maxime magni quos saepe sunt animi porro nam!
+				</Text>
+				<Divider my="sm" />
+				<Button variant="gradient" gradient={{ from: "indigo", to: "blue" }}>
+					Click me!
+				</Button>
+			</Paper>
+		</Container>
+	);
 }
