8d2708857ddadf6ed4aa86d976106222bdfaa975
yunger <luisgalete6264@gmail.com>
2022-06-22 11:41:36 -0300

    feat(*): add website.config.js

---
+++ website.config.js
@@ -0,0 +1,10 @@
+export const websiteConfig = {
+	username: "Luís Galete",
+	social: {
+		github: "https://github.com/yunger7",
+		reddit: "https://www.reddit.com/user/yunger_",
+		steam: "https://steamcommunity.com/id/yunger/",
+		discord: "yunger#3461",
+		twitter: null,
+	},
+};
