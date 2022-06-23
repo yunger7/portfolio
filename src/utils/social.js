82b587da24088f00f07f4bac7be4e38f6b9c9c30
yunger <luisgalete6264@gmail.com>
2022-06-23 12:16:36 -0300

    feat(utils): add 'social' utils

---
+++ src/utils/social.js
@@ -0,0 +1,25 @@
+import {
+	BrandGithub as GithubIcon,
+	BrandReddit as RedditIcon,
+	BrandSteam as SteamIcon,
+	BrandDiscord as DiscordIcon,
+} from "tabler-icons-react";
+
+export const SOCIAL = {
+	github: {
+		name: "GitHub",
+		icon: GithubIcon,
+	},
+	reddit: {
+		name: "Reddit",
+		icon: RedditIcon,
+	},
+	steam: {
+		name: "Steam",
+		icon: SteamIcon,
+	},
+	discord: {
+		name: "Discord",
+		icon: DiscordIcon,
+	},
+};
