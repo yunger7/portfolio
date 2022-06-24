a111ed144eeae0146ddfd08c64e84b29c69ed684
yunger <luisgalete6264@gmail.com>
2022-06-24 14:32:57 -0300

    feat: add About section to landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,9 @@
-import { Container, Text, useMantineColorScheme } from "@mantine/core";
+import { useMantineColorScheme } from "@mantine/core";
 
 import { Header } from "@components";
-import { Home } from "@components/sections";
+import { Home, About } from "@components/sections";
+
+import { about } from "website.config"; // temp
 
 export default function LandingPage() {
 	const { colorScheme, toggleColorScheme } = useMantineColorScheme();
@@ -10,17 +12,7 @@ export default function LandingPage() {
 		<>
 			<Header />
 			<Home />
-			<Container size="xl" p="xl" sx={{ minHeight: "80vh" }}>
-				<Text>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nostrum
-					consequatur, laudantium inventore deleniti sunt saepe eaque distinctio
-					ad harum architecto eius consequuntur rem dicta aut, repudiandae illo.
-					Excepturi, eius. Lorem ipsum dolor sit amet consectetur adipisicing
-					elit. Suscipit, ratione cum aperiam maxime laboriosam soluta eveniet
-					cumque unde nostrum quam quia fuga eos assumenda quisquam sit
-					similique, mollitia temporibus dignissimos?
-				</Text>
-			</Container>
+			<About content={about} />
 		</>
 	);
 }
