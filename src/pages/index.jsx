ea7244c1a12ee098246ea361da019d530267c483
yunger <luisgalete6264@gmail.com>
2022-06-24 18:12:37 -0300

    feat(landing page): add getStaticProps

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,48 @@
 import { useMantineColorScheme } from "@mantine/core";
 
+import { fetchGithubInfo, formatCommits } from "@lib";
+
 import { Header } from "@components";
 import { Home, About } from "@components/sections";
 
-import { about } from "website.config"; // temp
+import { about, firstCommitDate, location, bio } from "website.config";
 
-export default function LandingPage() {
+export default function LandingPage({ bio, about }) {
 	const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
 	return (
 		<>
 			<Header />
-			<Home />
+			<Home bio={bio} />
 			<About content={about} />
 		</>
 	);
+}
+
+export async function getStaticProps() {
+	const GITHUB_ACCESS_TOKEN = process.env["GITHUB_ACCESS_TOKEN"];
+	const { totalCommits, totalRepos } = await fetchGithubInfo(
+		GITHUB_ACCESS_TOKEN
+	);
+
+	let updatedBio = bio;
+	const programmingAge =
+		new Date().getFullYear() - firstCommitDate.getFullYear();
+	updatedBio = updatedBio.replace("{{ LOCATION }}", location);
+	updatedBio = updatedBio.replace("{{ PROGRAMMING_AGE }}", programmingAge);
+
+	let updatedAbout = about;
+	updatedAbout = updatedAbout.replace(
+		"{{ COMMITS }}",
+		formatCommits(totalCommits)
+	);
+	updatedAbout = updatedAbout.replace("{{ REPOS }}", totalRepos);
+
+	return {
+		props: {
+			about: updatedAbout,
+			bio: updatedBio,
+		},
+		revalidate: 60 * 60 * 24,
+	};
 }
