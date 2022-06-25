ace22313d08845ad167f5b6f574b48adf4a5935d
yunger <luisgalete6264@gmail.com>
2022-06-24 21:47:21 -0300

    fix(css): fix About section height

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,6 +3,7 @@ import {
 	Center,
 	Text,
 	Title,
+	Box,
 	Grid,
 	Card,
 	createStyles,
@@ -142,108 +143,114 @@ export function About({ content }) {
 	const { classes } = useStyles();
 
 	return (
-		<Container size="xl" p="xl" mt="20vh" sx={{ minHeight: "80vh" }}>
-			<Grid
-				justify="space-between"
-				sx={{ width: "100%", height: "100%", minHeight: "80vh" }}
-			>
-				<Grid.Col className={classes.react} sm={3} md={4}>
-					<Skill size="medium" icon={ReactIcon} animationDelay={1000} />
-				</Grid.Col>
-				<Grid.Col className={classes.javascript} sm={4} md={4}>
-					<Skill
-						size="large"
-						icon={JavascriptIcon}
-						animationVariant="2"
-						sx={{
-							[theme.fn.smallerThan("sm")]: { marginLeft: "40%" },
-							[theme.fn.smallerThan("xs")]: { marginLeft: "unset" },
-						}}
-					/>
-				</Grid.Col>
-				<Grid.Col className={classes.html} sm={3} md={4}>
-					<Skill
-						size="medium"
-						icon={HtmlIcon}
-						animationDelay={1500}
-						animationVariant="3"
-						sx={{
-							[theme.fn.smallerThan("sm")]: { marginRight: "40%" },
-							[theme.fn.smallerThan("xs")]: { marginRight: "unset" },
-						}}
-					/>
-				</Grid.Col>
-				<Grid.Col className={classes.css} sm={2}>
-					<Skill
-						size="small"
-						icon={CssIcon}
-						animationDelay={500}
-						animationVariant="2"
-					/>
-				</Grid.Col>
-				<Grid.Col md={6} my="lg">
-					<Center sx={{ width: "100%", height: "100%" }}>
-						<Card
-							radius="lg"
-							shadow="xl"
-							p="xl"
-							sx={theme => ({
-								maxWidth: theme.breakpoints.xs,
-								margin: "auto",
-								textAlign: "center",
-							})}
-						>
-							<CodeIcon size={42} />
-							<Title order={2} weight="bold" mb="lg">
-								Programação é uma{" "}
-								<Text
-									inherit
-									component="span"
-									variant="gradient"
-									gradient={{ from: "blue", to: "cyan" }}
-								>
-									ferramenta
-								</Text>
-								.
-							</Title>
-							<Text>{content}</Text>
-						</Card>
-					</Center>
-				</Grid.Col>
-				<Grid.Col className={classes.firebase} sm={2}>
-					<Skill size="small" icon={FirebaseIcon} animationDelay={1500} />
-				</Grid.Col>
-				<Grid.Col className={classes.vercel} sm={3} md={4}>
-					<Skill
-						size="medium"
-						icon={VercelIcon}
-						animationVariant="2"
-						sx={{
-							[theme.fn.smallerThan("sm")]: { marginLeft: "40%" },
-							[theme.fn.smallerThan("xs")]: { marginLeft: "unset" },
-						}}
-					/>
-				</Grid.Col>
-				<Grid.Col className={classes.nextjs} sm={4} md={4}>
-					<Skill
-						size="large"
-						icon={NextjsIcon}
-						animationDelay={1000}
-						sx={{
-							[theme.fn.smallerThan("sm")]: { marginRight: "40%" },
-							[theme.fn.smallerThan("xs")]: { marginRight: "unset" },
-						}}
-					/>
-				</Grid.Col>
-				<Grid.Col className={classes.vscode} sm={3} md={4}>
-					<Skill
-						size="medium"
-						icon={VscodeIcon}
-						animationDelay={500}
-						animationVariant="3"
-					/>
-				</Grid.Col>
-			</Grid>
-		</Container>
+		<Box
+			sx={{
+				position: "relative",
+				width: "100%",
+				height: "100%",
+				minHeight: "90vh",
+			}}
+		>
+			<Container size="xl" py="10vh" sx={{ width: "100%", height: "100%" }}>
+				<Grid justify="space-between" sx={{ width: "100%", height: "100%" }}>
+					<Grid.Col className={classes.react} sm={3} md={4}>
+						<Skill size="medium" icon={ReactIcon} animationDelay={1000} />
+					</Grid.Col>
+					<Grid.Col className={classes.javascript} sm={4} md={4}>
+						<Skill
+							size="large"
+							icon={JavascriptIcon}
+							animationVariant="2"
+							sx={{
+								[theme.fn.smallerThan("sm")]: { marginLeft: "40%" },
+								[theme.fn.smallerThan("xs")]: { marginLeft: "unset" },
+							}}
+						/>
+					</Grid.Col>
+					<Grid.Col className={classes.html} sm={3} md={4}>
+						<Skill
+							size="medium"
+							icon={HtmlIcon}
+							animationDelay={1500}
+							animationVariant="3"
+							sx={{
+								[theme.fn.smallerThan("sm")]: { marginRight: "40%" },
+								[theme.fn.smallerThan("xs")]: { marginRight: "unset" },
+							}}
+						/>
+					</Grid.Col>
+					<Grid.Col className={classes.css} sm={2}>
+						<Skill
+							size="small"
+							icon={CssIcon}
+							animationDelay={500}
+							animationVariant="2"
+						/>
+					</Grid.Col>
+					<Grid.Col md={6} my="lg">
+						<Center sx={{ width: "100%", height: "100%" }}>
+							<Card
+								radius="lg"
+								shadow="xl"
+								p="xl"
+								sx={theme => ({
+									maxWidth: theme.breakpoints.xs,
+									margin: "auto",
+									textAlign: "center",
+								})}
+							>
+								<CodeIcon size={42} />
+								<Title order={2} weight="bold" mb="lg">
+									Programação é uma{" "}
+									<Text
+										inherit
+										component="span"
+										variant="gradient"
+										gradient={{ from: "blue", to: "cyan" }}
+									>
+										ferramenta
+									</Text>
+									.
+								</Title>
+								<Text>{content}</Text>
+							</Card>
+						</Center>
+					</Grid.Col>
+					<Grid.Col className={classes.firebase} sm={2}>
+						<Skill size="small" icon={FirebaseIcon} animationDelay={1500} />
+					</Grid.Col>
+					<Grid.Col className={classes.vercel} sm={3} md={4}>
+						<Skill
+							size="medium"
+							icon={VercelIcon}
+							animationVariant="2"
+							sx={{
+								[theme.fn.smallerThan("sm")]: { marginLeft: "40%" },
+								[theme.fn.smallerThan("xs")]: { marginLeft: "unset" },
+							}}
+						/>
+					</Grid.Col>
+					<Grid.Col className={classes.nextjs} sm={4} md={4}>
+						<Skill
+							size="large"
+							icon={NextjsIcon}
+							animationDelay={1000}
+							sx={{
+								[theme.fn.smallerThan("sm")]: { marginRight: "40%" },
+								[theme.fn.smallerThan("xs")]: { marginRight: "unset" },
+							}}
+						/>
+					</Grid.Col>
+					<Grid.Col className={classes.vscode} sm={3} md={4}>
+						<Skill
+							size="medium"
+							icon={VscodeIcon}
+							animationDelay={500}
+							animationVariant="3"
+						/>
+					</Grid.Col>
+				</Grid>
+			</Container>
+		</Box>
 	);
 }
