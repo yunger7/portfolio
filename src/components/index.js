480f6641b0755bd000bf0c6f21a3551dc8d1a9fc
yunger <luisgalete6264@gmail.com>
2022-06-26 17:32:06 -0300

    feat: add ImageShowcase component

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export * from "./GlassCard";
 export * from "./GradientBackground";
 export * from "./Header";
 export * from "./IconBadge";
+export * from "./ImageShowcase";
 export * from "./ProjectCard";
 export * from "./Skill";
 export * from "./WaveBackground";
