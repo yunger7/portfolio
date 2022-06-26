eaf83c6946603c064e19612977080b355067363c
yunger <luisgalete6264@gmail.com>
2022-06-26 17:32:51 -0300

    feat(projectcard): add project details to ProjectCard component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,40 @@
-import Image from "next/image";
-import { Center, Box } from "@mantine/core";
+import NextImage from "next/image";
 
-import { GlassCard } from "@/components";
+import {
+	Center,
+	Box,
+	Title,
+	Text,
+	Group,
+	Button,
+	Card,
+	Divider,
+	useMantineTheme,
+} from "@mantine/core";
+import { useModals } from "@mantine/modals";
+
+import {
+	Archive as ArchivedIcon,
+	Check as ActiveIcon,
+	BrandGithub as GithubIcon,
+	ExternalLink as ExternalLinkIcon,
+	Scale as LicenseIcon,
+} from "tabler-icons-react";
+
+import { GlassCard, IconBadge, ImageShowcase } from "@/components";
 
 export function ProjectCard({ project }) {
 	const { title, logo } = project;
 
+	const theme = useMantineTheme();
+	const modals = useModals();
+
 	function renderLogo() {
 		switch (logo.type) {
 			case "image":
-				return <Image src={logo.image} alt={title} width={64} height={64} />;
+				return (
+					<NextImage src={logo.image} alt={title} width={64} height={64} />
+				);
 			case "icon":
 				return (
 					<Box
@@ -30,6 +55,22 @@ export function ProjectCard({ project }) {
 		}
 	}
 
+	function showDetails() {
+		modals.openModal({
+			size: "xl",
+			withCloseButton: false,
+			padding: 0,
+			overlayColor:
+				theme.colorScheme === "light"
+					? theme.colors.gray[2]
+					: theme.colors.dark[9],
+			overlayOpacity: 0.25,
+			overlayBlur: 3,
+			centered: true,
+			children: <Details project={project} />,
+		});
+	}
+
 	return (
 		<Box
 			sx={{
@@ -44,9 +85,115 @@ export function ProjectCard({ project }) {
 				},
 			}}
 		>
-			<GlassCard shadow="sm" backgroundOpacity={0.1}>
+			<GlassCard shadow="sm" backgroundOpacity={0.1} onClick={showDetails}>
 				<Center sx={{ width: "100%", height: "100%" }}>{renderLogo()}</Center>
 			</GlassCard>
 		</Box>
+	);
+}
+
+function Details({ project }) {
+	const {
+		title,
+		description,
+		status,
+		source,
+		href,
+		license,
+		images,
+		technologies,
+	} = project;
+
+	return (
+		<Card
+			p="xl"
+			sx={theme => ({
+				[theme.fn.smallerThan("xs")]: { padding: theme.spacing.md },
+			})}
+		>
+			{images.length > 0 && <ImageShowcase images={images} />}
+			<Group mb="xs" mt="xl">
+				<Status variant={status} />
+				{license && (
+					<IconBadge
+						variant="outline"
+						radius="sm"
+						icon={<LicenseIcon />}
+						ActionIconProps={{ color: "blue" }}
+					>
+						{license}
+					</IconBadge>
+				)}
+			</Group>
+			<Title order={3} mb="xs">
+				{title}
+			</Title>
+			<Text>{description}</Text>
+			{technologies.length > 0 && (
+				<Group mt="sm">
+					{technologies.map(({ name, icon }) => (
+						<IconBadge icon={icon} ActionIconProps={{ color: "blue" }}>
+							{name}
+						</IconBadge>
+					))}
+				</Group>
+			)}
+			{(source || href) && (
+				<>
+					<Divider my="lg" />
+					<Group>
+						{source && (
+							<Button
+								component="a"
+								href={source}
+								target="_blank"
+								variant={href ? "outline" : "filled"}
+								leftIcon={<GithubIcon />}
+							>
+								Código fonte
+							</Button>
+						)}
+						{href && (
+							<Button
+								component="a"
+								href={href}
+								target="_blank"
+								leftIcon={<ExternalLinkIcon />}
+							>
+								Website
+							</Button>
+						)}
+					</Group>
+				</>
+			)}
+		</Card>
+	);
+}
+
+function Status({ variant }) {
+	const variants = {
+		active: {
+			text: "Ativo",
+			color: "green",
+			icon: <ActiveIcon />,
+		},
+		archived: {
+			text: "Arquivado",
+			color: "orange",
+			icon: <ArchivedIcon />,
+		},
+	};
+
+	return (
+		<IconBadge
+			color={variants[variant].color}
+			radius="sm"
+			icon={variants[variant].icon}
+			ActionIconProps={{
+				color: variants[variant].color,
+			}}
+		>
+			{variants[variant].text}
+		</IconBadge>
 	);
 }
