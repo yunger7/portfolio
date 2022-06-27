8008aec4feb96cf4fc8a98de30851f439d89bf76
yunger <luisgalete6264@gmail.com>
2022-06-27 18:16:29 -0300

    refactor(iconbadge): change component to span

--- src/components/IconBadge.jsx
+++ src/components/IconBadge.jsx
@@ -3,6 +3,7 @@ import { ActionIcon, Badge } from "@mantine/core";
 export function IconBadge({ children, icon, ActionIconProps, ...props }) {
 	const Icon = (
 		<ActionIcon
+			component="span"
 			variant="transparent"
 			size="xs"
 			radius="xl"
