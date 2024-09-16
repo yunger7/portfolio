7a47fe82c60568ef99346a574a28038cbd377a26
Luis Galete <luisgalete6264@gmail.com>
2024-09-16 08:26:29 -0300

    refactor: improve language toggle
    save changes to local storage and set default value according to user preference

--- src/contexts/Language.jsx
+++ src/contexts/Language.jsx
@@ -1,4 +1,5 @@
-import { createContext, useEffect, useState } from "react";
+import { useLocalStorage } from "@mantine/hooks";
+import { createContext, useEffect } from "react";
 
 export const LanguageContext = createContext({
 	language: "",
@@ -6,7 +7,21 @@ export const LanguageContext = createContext({
 });
 
 export function LanguageProvider({ children }) {
-	const [language, setLanguage] = useState("pt");
+	const [language, setLanguage] = useLocalStorage({
+        key: "language",
+        defaultValue: "pt",
+    });
+
+    useEffect(() => {
+        const isLanguageInitialized = localStorage.getItem("isLanguageInitialized");
+
+        if (typeof window !== "undefined" && isLanguageInitialized !== "true") {
+            const language = navigator.language || navigator.userLanguage;
+            setLanguage(language.includes("en") ? "en" : "pt");
+
+            localStorage.setItem("isLanguageInitialized", "true");
+        }
+    }, []);
 
 	function toggleLanguage() {
 		setLanguage(lang => (lang == "pt" ? "en" : "pt"));
